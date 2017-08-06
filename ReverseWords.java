6e97ca5fb2216621fe6d86bff208c934869592fd
Chase Lirette <chaselirette@yahoo.com>
2017-08-06 17:39:14 -0500

    Modified the algorithm to run in linear time. Optimal solution

--- ReverseWords.java
+++ ReverseWords.java
@@ -2,34 +2,39 @@ import java.util.Scanner;
 
 /**
  * Simple class that reverses the words in a String.
+ * Runs in linear time as of version 2.
  * @Author Chase Lirette
- * @Version 4/15/17
+ * @Version 8/6/17 
  */
 public class ReverseWords {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		StringBuilder sb = new StringBuilder();
 		System.out.println("Enter a string to be reversed.");
-		String testString = "";
-		testString = input.nextLine();
+		String inputString = "";
+		inputString = input.nextLine().trim();
+		System.out.println(reverseString(inputString));
+	}
+
+	public static String reverseString(String testString) {
+		StringBuilder sb = new StringBuilder();
 		int end = testString.length() - 1;
-		int start = end;
-		for (int i = testString.length() - 1; i >= 0; i--){
-			if (testString.charAt(i) == ' ' || i == 0) {
-				if (i == 0) {
-					start = i;
-				} else {
-					start = i + 1;
+		int chars = 0;
+		int spaceOffset = 0;
+		for (int i=end; i>=0; i--) {
+			if (testString.charAt(i) == ' ') {
+				spaceOffset++;
+				if (testString.charAt(i-1) == ' ') {
+					continue;
 				}
-				for (int j = start; j <= end; j++){
-					sb.append(testString.charAt(j));
-				}
-				if (i != 0) {
-					sb.append(" ");
-				}
-				end = i-1;
+				sb.append(testString.substring(i+spaceOffset, i+spaceOffset+chars));
+				sb.append(' ');
+				chars = 0;
+				spaceOffset = 0;
+			} else {
+				chars++;
 			}
 		}
-		System.out.println(sb.toString());
+		sb.append(testString.substring(0, chars));
+		return sb.toString();
 	}
 }
